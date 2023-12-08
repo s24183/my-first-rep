4b5dcf7c19e063c368d2779835d44d6b539edf1f
Jonatant NOSTHE <125766721+s24183@users.noreply.github.com>
2023-12-08 02:40:29 +0100

    Update Fibonacci.java

--- Lab1/src/a_Introductory/Fibonacci.java
+++ Lab1/src/a_Introductory/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
 		switch (n) {
 			case 0: return 0;
 			case 1: return 1;
-
+			//It should not have +1 after the fib function because it will always increase by one rather than the actual values.
 			default: return (fib(n - 1) + fib(n - 2)) ;
 		}
 	}
